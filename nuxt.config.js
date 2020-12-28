57dc33761e252587b6053b7d62a65ed6dc2a870e
Ahmad Fauzan <ahmadfauzaan613@gmail.com>
2020-12-28 10:24:56 +0700

    hosting

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,82 +1,78 @@
 export default {
 
-  generate: {
-    fallback: true
-  },
+    generate: {
+        fallback: true
+    },
 
-  // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
-  ssr: false,
+    // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
+    ssr: false,
 
 
-  // Target (https://go.nuxtjs.dev/config-target)
-  target: 'static',
+    // Target (https://go.nuxtjs.dev/config-target)
+    target: 'static',
 
-  // Global page headers (https://go.nuxtjs.dev/config-head)
-  head: {
-    title: 'Pendanaan UMKM',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+    // Global page headers (https://go.nuxtjs.dev/config-head)
+    head: {
+        title: 'Pendanaan UMKM',
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { hid: 'description', name: 'description', content: '' }
+        ],
+        link: [
+            { rel: 'icon', type: 'image/x-icon', href: '/money-bag.svg' },
+            {
+                rel: 'stylesheet',
+                href: 'https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap',
+            },
+        ],
+    },
+
+
+    // Global CSS (https://go.nuxtjs.dev/config-css)
+    css: [],
+
+    // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
+    plugins: [],
+
+    // Auto import components (https://go.nuxtjs.dev/config-components)
+    components: true,
+
+    // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+    buildModules: [
+        // https://go.nuxtjs.dev/tailwindcss
+        '@nuxtjs/tailwindcss',
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/money-bag.svg' },
-      { rel: 'stylesheet', 
-        href: 
-        'https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap', 
-      },
+
+    // Modules (https://go.nuxtjs.dev/config-modules)
+    modules: [
+        // https://go.nuxtjs.dev/axios
+        '@nuxtjs/axios',
+        // https://auth.nuxtjs.org/guide/setup.html
+        '@nuxtjs/auth',
     ],
-  },
 
-  
-  // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [
-  ],
+    // Axios module configuration (https://go.nuxtjs.dev/config-axios)
+    axios: {
+        baseURL: 'https://gentle-sea-19868.herokuapp.com/'
+    },
 
-  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [
-  ],
+    auth: {
+        strategies: {
+            local: {
+                endpoints: {
+                    login: { url: '/api/v1/sessions', method: 'post', propertyName: 'data.token' },
+                    logout: false,
+                    user: { url: '/api/v1/user/fetch', method: 'get', propertyName: 'data' }
+                },
+                // tokenRequired: true,
+                // tokenType: 'bearer',
+                // globalToken: true,
+                // autoFetchUser: true
+            }
+        }
+    },
 
-  // Auto import components (https://go.nuxtjs.dev/config-components)
-  components: true,
-
-  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
-  buildModules: [
-    // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss',
-  ],
-
-  // Modules (https://go.nuxtjs.dev/config-modules)
-  modules: [
-    // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
-    // https://go.nuxtjs.dev/pwa
-    '@nuxtjs/pwa',
-    // https://auth.nuxtjs.org/guide/setup.html
-    '@nuxtjs/auth',
-  ],
-
-  // Axios module configuration (https://go.nuxtjs.dev/config-axios)
-  axios: {
-    baseURL: 'http://localhost:8080'
-  },
-
-  auth: {
-    strategies: {
-      local: {
-        endpoints: {
-          login: { url: '/api/v1/sessions', method: 'post', propertyName: 'data.token' },
-          logout: false,
-          user: { url: '/api/v1/user/fetch', method: 'get', propertyName: 'data' }
-        },
-        // tokenRequired: true,
-        // tokenType: 'bearer',
-        // globalToken: true,
-        // autoFetchUser: true
-      }
-    }
-  },
-
-  // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {},
+    // Build Configuration (https://go.nuxtjs.dev/config-build)
+    build: {},
 }
