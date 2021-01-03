68fb2a86c018f54b8cac6b02c3b7edf71b4a3e82
Ahmad Fauzan <ahmadfauzaan613@gmail.com>
2021-01-04 04:43:53 +0700

    status2

--- pages/dashboard/projects/_id/index.vue
+++ pages/dashboard/projects/_id/index.vue
@@ -114,7 +114,7 @@
                 {{transaction.nama}}
                 </div>
                 <p class="text-sm text-gray-600 flex items-center mb-2">
-                Rp. {{new Intl.NumberFormat().format(transaction.amount)}} &middot; {{transaction.status}}
+                Rp. {{new Intl.NumberFormat().format(transaction.amount)}} &middot; {{transaction.Status}}
                 </p>
             </div>
             </div>
