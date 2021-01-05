f72b553e24ba5374b558d89c0ada3894ec3f9c90
Ahmad Fauzan <ahmadfauzaan613@gmail.com>
2021-01-05 11:33:37 +0700

    fixed

--- pages/dashboard/projects/create.vue
+++ pages/dashboard/projects/create.vue
@@ -39,7 +39,7 @@
                     <input
                     class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                     type="text"
-                    placeholder="Contoh: Demi Gunpla Demi Istri"
+                    placeholder="Contoh: Dapur Bunda"
                     v-model="campaign.nama_campaign"
                     />
                 </div>
