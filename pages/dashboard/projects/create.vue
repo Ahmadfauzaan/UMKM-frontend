aa5e8ded25cf358b2a1a70d68f8b77aa6d3003a0
Ahmad Fauzan <ahmadfauzaan613@gmail.com>
2021-01-07 06:28:39 +0700

    fixed

--- pages/dashboard/projects/create.vue
+++ pages/dashboard/projects/create.vue
@@ -77,7 +77,31 @@
                     </label>
                     <input
                     class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                    type="text"
+                    
+                    placeholder="Contoh: Mendapat keuntungan usaha 10%"
+                    v-model="campaign.perks"
+                    />
+                    <input
+                    class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                    
+                    placeholder="Contoh: Mendapat keuntungan usaha 10%"
+                    v-model="campaign.perks"
+                    />
+                    <input
+                    class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                    
+                    placeholder="Contoh: Mendapat keuntungan usaha 10%"
+                    v-model="campaign.perks"
+                    />
+                    <input
+                    class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                    
+                    placeholder="Contoh: Mendapat keuntungan usaha 10%"
+                    v-model="campaign.perks"
+                    />
+                    <input
+                    class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                    
                     placeholder="Contoh: Mendapat keuntungan usaha 10%"
                     v-model="campaign.perks"
                     />
