b7fa1a78fc7715e2d3b77a21e0281bc877a071d0
Ahmad Fauzan <ahmadfauzaan613@gmail.com>
2021-01-07 04:51:03 +0700

    fixed

--- pages/dashboard/projects/create.vue
+++ pages/dashboard/projects/create.vue
@@ -82,58 +82,6 @@
                     v-model="campaign.perks"
                     />
                 </div>
-                 <div class="w-full px-3">
-                    <label
-                    class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                    >
-                    Apa yang akan didapat investor
-                    </label>
-                    <input
-                    class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                    type="text"
-                    placeholder="Contoh: Mendapat keuntungan usaha 10%"
-                    v-model="campaign.perks"
-                    />
-                </div>
-                 <div class="w-full px-3">
-                    <label
-                    class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                    >
-                    Apa yang akan didapat investor
-                    </label>
-                    <input
-                    class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                    type="text"
-                    placeholder="Contoh: Mendapat keuntungan usaha 10%"
-                    v-model="campaign.perks"
-                    />
-                </div>
-                 <div class="w-full px-3">
-                    <label
-                    class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                    >
-                    Apa yang akan didapat investor
-                    </label>
-                    <input
-                    class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                    type="text"
-                    placeholder="Contoh: Mendapat keuntungan usaha 10%"
-                    v-model="campaign.perks"
-                    />
-                </div>
-                 <div class="w-full px-3">
-                    <label
-                    class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                    >
-                    Apa yang akan didapat investor
-                    </label>
-                    <input
-                    class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                    type="text"
-                    placeholder="Contoh: Mendapat keuntungan usaha 10%"
-                    v-model="campaign.perks"
-                    />
-                </div>
                 <div class="w-full px-3">
                     <label
                     class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
