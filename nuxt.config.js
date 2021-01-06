951e756d4f1140adc02282c733885cfd48474431
Ahmad Fauzan <ahmadfauzaan613@gmail.com>
2021-01-06 10:09:44 +0700

    fixed

--- nuxt.config.js
+++ nuxt.config.js
@@ -54,7 +54,7 @@ export default {
 
     // Axios module configuration (https://go.nuxtjs.dev/config-axios)
     axios: {
-        baseURL: 'http://localhost:8080'
+        baseURL: 'https://gentle-sea-19868.herokuapp.com'
     },
 
     auth: {
